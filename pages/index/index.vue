7dbadd8f4e6f996ea197ab0224d1c11866cf02af
MimoriTyann <MimoriTyann@gmail.com>
2019-04-27 19:05:03 +0800

    初始化结构

---
+++ pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <section>
+    <div>index</div>
+    <nuxt-link :to="{name: 'index-point'}">Change</nuxt-link>
+  </section>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped lang="scss">
+.header {
+  background: #666;
+  color: #000;
+  height: 50px;
+}
+</style>
