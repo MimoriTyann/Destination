6c5114ebef4d6770ed63688fa21055af72163aa4
MimoriTyann <38640496+MimoriTyann@users.noreply.github.com>
2019-07-02 15:51:04 +0800

    title

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ module.exports = {
    ** Headers of the page
    */
   head: {
-    title: pkg.name,
+    title: 'Destination',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no' },
