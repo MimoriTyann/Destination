36c59b8c2b6df3af096777fca970dee71c6de1b3
MimoriTyann <38640496+MimoriTyann@users.noreply.github.com>
2019-07-05 16:28:02 +0800

    apple-mobile-web-app-capable

--- nuxt.config.js
+++ nuxt.config.js
@@ -15,7 +15,8 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no' },
-      { hid: 'description', name: 'description', content: pkg.description }
+      { hid: 'description', name: 'description', content: pkg.description },
+      { name: 'apple-mobile-web-app-capable', content: 'yes' }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }, { rel: 'apple-touch-icon', href: '/favicon.png' }]
   },
