f4f9faade32f76237ee1e7eaa43b787a332c0db1
MimoriTyann <38640496+MimoriTyann@users.noreply.github.com>
2019-07-05 15:45:02 +0800

    change icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }]
   },
 
   /*
