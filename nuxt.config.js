8edf1190735b2ba45b23e1aec60a7da450f9a0c1
MimoriTyann <38640496+MimoriTyann@users.noreply.github.com>
2019-07-05 15:55:52 +0800

    apple-touch-icon

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,7 +17,7 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no' },
       { hid: 'description', name: 'description', content: pkg.description }
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }]
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }, { rel: 'apple-touch-icon', href: '/favicon.png' }]
   },
 
   /*
